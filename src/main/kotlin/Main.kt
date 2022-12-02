c1e0fec93a16b9a47b049b65096ea8f352af3f5d
Feruz <feruabduhalilov3@gmail.com>
2022-12-03 00:26:23 +0500

    init

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+/*    var a:Double = 5.0
+    a = 1.0
+    println(a)
+    println("\na="+a)
+    println("a=$a")
+    var b:Int = 0
+    b=3
+*/
+    var d:Double=12.0
+    println(d/3)
+    val f:Float = 12.0f
+    println(f/3)
+    val i:Int=12
+    println(i/3)
+}
